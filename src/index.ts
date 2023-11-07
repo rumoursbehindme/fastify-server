07268958ccf430ca83032837f4278e55ff74e820
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-11-07 16:42:41 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify({
+    logger: {
+        base: undefined,
+    
+    }
+});
+
+app.get('/', (req, reply) => {
+    reply.send(req.ip);
+})
+app.listen({ port: 8080 });
