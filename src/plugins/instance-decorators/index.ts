baba8885fb2f5d59543eb8c80d82f30a1dd99e87
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-11-15 19:19:53 +0530

    feat: added registering multiple plugins

---
+++ src/plugins/instance-decorators/index.ts
@@ -0,0 +1,10 @@
+// import { FastifyPluginAsync } from "fastify";
+// import plugin from 'fastify-plugin';
+// import registerMultiplePlugins from "./utils/register-multiple-plugins";
+
+
+// const instanceDecoratorsPlugin: FastifyPluginAsync = async function (instance) {
+//     instance
+// }
+
+// // export default plugin(instanceDecoratorsPlugin);
---
+++ src/plugins/utils/register-multiple-plugins.ts
@@ -0,0 +1,25 @@
+import plugin from 'fastify-plugin';
+import { FastifyPluginAsync } from 'fastify';
+import { IPlugins } from './types';
+
+declare module 'fastify' {
+    interface FastifyInstance {
+        registerPlugins(plugins: Array<IPlugins>): Promise<void>
+    }
+}
+
+const registerMultiplePlugins: FastifyPluginAsync = async function registerMultiplePlugins(instance) {
+
+    instance.decorate('registerPlugins', async function (plugins: Array<IPlugins>) {
+        try {
+            plugins.forEach(async ({ plugin, options }) => {
+                await instance.register(plugin, options)
+            })
+        }
+        catch (error) {
+            console.log(error)
+        }
+    });
+}
+
+export default plugin(registerMultiplePlugins);
